eef15628febce61398b49764d763392fab4e4a1f
Angel-Fer <angelferri187@gmail.com>
2025-05-10 20:58:33 -0300

    Update canal.php

--- canal.php
+++ canal.php
@@ -21,7 +21,20 @@
 
                 <?php
                 // Consulta combinada nombre + link
-                include 'partes/control.php';
+                $linkcanal = $conexion->query("SELECT nombre, link, icono FROM `recopilacion`");
+                foreach ($linkcanal as $index => $canal) {
+                    $nombre = htmlspecialchars($canal['nombre']);
+                    $link = htmlspecialchars($canal['link']);
+                    $icono = htmlspecialchars($canal['icono']);
+
+                    echo "<button class='canal-boton' data-link='$link'>";
+                    echo '<div class="opcanal">
+                            <p>'.$nombre.'</p>
+                            <img src="'.$icono.'" alt="Icono">
+                        </div>';
+                    echo "</button>";
+                }
+
                 ?>
 
                 <script>
